0e699824581d863b568b569d108bc74aefe09ef9
wuxinzhong <wuxinzhong@qq.com>
2019-11-25 10:04:55 +0800

    tijiao

--- wuxinzhong20191121/src/main/java/com/bw/wuxinzhong20191121/view/adapter/ShopCarAdapter.java
+++ wuxinzhong20191121/src/main/java/com/bw/wuxinzhong20191121/view/adapter/ShopCarAdapter.java
@@ -15,12 +15,6 @@ import com.facebook.drawee.view.SimpleDraweeView;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * <p>文件描述：<p>
- * <p>作者：吴新仲<p>
- * <p>创建时间：2019/11/21/021<p>
- * <p>更改时间：2019/11/21/021<p>
- */
 public class ShopCarAdapter extends BaseExpandableListAdapter implements View.OnClickListener {
 
     private List<ShopCarBean.ResultBean> mGroupList = new ArrayList<>();
--- wuxinzhong20191121/src/main/java/com/bw/wuxinzhong20191121/view/myview/AdderView.java
+++ wuxinzhong20191121/src/main/java/com/bw/wuxinzhong20191121/view/myview/AdderView.java
@@ -12,12 +12,7 @@ import android.widget.Toast;
 import com.bw.wuxinzhong20191121.R;
 import com.bw.wuxinzhong20191121.model.bean.ShopCarBean;
 
-/**
- * <p>文件描述：<p>
- * <p>作者：吴新仲<p>
- * <p>创建时间：2019/11/21/021<p>
- * <p>更改时间：2019/11/21/021<p>
- */
+
 public class AdderView extends LinearLayout implements View.OnClickListener {
 
     public TextView add_jian, add_jia;
